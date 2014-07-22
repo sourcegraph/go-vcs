d4847822b8db8a197fe11ea4b3e889dde6bac25a
Colton Baker <github@netrefuge.net>
2014-07-21 23:06:26 -0400

    Get rid of GitRepositoryNative

--- vcs/vcs.go
+++ vcs/vcs.go
@@ -1,10 +1,17 @@
 package vcs
 
 import (
+	"io"
 	"os"
 	"path/filepath"
 	"time"
 )
+
+type nopCloser struct {
+	io.ReadSeeker
+}
+
+func (nc nopCloser) Close() error { return nil }
 
 func getModTime(dir, path string) time.Time {
 	stat, err := os.Stat(filepath.Join(dir, path))
