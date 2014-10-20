808806275f4ac0ef0a5f8e0236b7d0d8d1e2782e
Quinn Slack <qslack@qslack.com>
2014-10-19 22:10:25 -0700

    don't provide default return impls in MockRepository (makes it easy to accidentally rely on them in a test)

--- vcs/testing/mock_repository.go
+++ vcs/testing/mock_repository.go
@@ -22,57 +22,33 @@ type MockRepository struct {
 var _ vcs.Repository = MockRepository{}
 
 func (r MockRepository) ResolveRevision(spec string) (vcs.CommitID, error) {
-	if r.ResolveRevision_ == nil {
-		return "", nil
-	}
 	return r.ResolveRevision_(spec)
 }
 
 func (r MockRepository) ResolveTag(name string) (vcs.CommitID, error) {
-	if r.ResolveTag_ == nil {
-		return "", nil
-	}
 	return r.ResolveTag_(name)
 }
 
 func (r MockRepository) ResolveBranch(name string) (vcs.CommitID, error) {
-	if r.ResolveBranch_ == nil {
-		return "", nil
-	}
 	return r.ResolveBranch_(name)
 }
 
 func (r MockRepository) Branches() ([]*vcs.Branch, error) {
-	if r.Branches_ == nil {
-		return nil, nil
-	}
 	return r.Branches_()
 }
 
 func (r MockRepository) Tags() ([]*vcs.Tag, error) {
-	if r.Tags_ == nil {
-		return nil, nil
-	}
 	return r.Tags_()
 }
 
 func (r MockRepository) GetCommit(id vcs.CommitID) (*vcs.Commit, error) {
-	if r.GetCommit_ == nil {
-		return nil, nil
-	}
 	return r.GetCommit_(id)
 }
 
 func (r MockRepository) Commits(opt vcs.CommitsOptions) ([]*vcs.Commit, uint, error) {
-	if r.Commits_ == nil {
-		return nil, 0, nil
-	}
 	return r.Commits_(opt)
 }
 
 func (r MockRepository) FileSystem(at vcs.CommitID) (vfs.FileSystem, error) {
-	if r.FileSystem_ == nil {
-		return nil, nil
-	}
 	return r.FileSystem_(at)
 }
