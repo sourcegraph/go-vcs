113e47e6092054371d860fbd087dadac099b00d2
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-06-10 22:28:39 -0700

    vcs/util: special-case "/" to return "." in Rel

--- vcs/util/path.go
+++ vcs/util/path.go
@@ -5,5 +5,8 @@ import "strings"
 // Rel strips the leading "/" prefix from the path string, effectively turning
 // an absolute path into one relative to the root directory.
 func Rel(path string) string {
+	if path == "/" {
+		return "."
+	}
 	return strings.TrimPrefix(path, "/")
 }
